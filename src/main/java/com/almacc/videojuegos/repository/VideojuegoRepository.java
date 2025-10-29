1b2ae86944609c39d6891df351b2b2cac483765b
Alma Carmona <almaccwork@gmail.com>
2025-10-29 19:42:40 +0100

    Parametros dinámicos con Thymeleaf

--- src/main/java/com/almacc/videojuegos/repository/VideojuegoRepository.java
+++ src/main/java/com/almacc/videojuegos/repository/VideojuegoRepository.java
@@ -9,8 +9,9 @@ import org.springframework.data.jpa.repository.Query;
 /**
  *
  * @author almac
- * @version 1.5
  * @since 2025-08-22
+ * @version 1.6
+ * @update 2025-10-29
  * Repositorio que se encarga de acceder a los datos de la base de datos. Se
  * inyecta automáticamente gracias a la Inversión de Control de Spring.
  */
@@ -31,9 +32,21 @@ public interface VideojuegoRepository extends JpaRepository<Videojuego, Integer>
      * En JPQL se consultan objetos (en vez de tablas), y atributos de las instancias (en lugar de columnas). 
      * Se pueden usar todas las clases que estén mapeadas con JPA.
      *
+     * Puedo utilizar la etiqueta (nativeQuery = true) de esta forma permito que Query admita SQL básico, pero no es lo más común.
+     * 
+     * Lo normal es poner la sentencia para obtener todas las instancias de la siguiente manera: from Videojuego vj order by vj.nombre 
      */
     @Query("select vj from Videojuego vj order by vj.nombre")
     List<Videojuego> findAllVj();
+    
+    /**
+     * Puedo agregar parámetros al métoro de Query, y para referenciarlos hay que utilizar:
+     * ?1, ?2, ?3, ..., ?n. Así sucesivamente.
+     * 
+     * Realiza la búsqueda por id del distribuidor y devuelve la Lista de Videojuegos ordenas por nombre.
+     */
+    @Query("from Videojuego vj where vj.distribuidor.id_db = ?1 order by nombre")
+    List<Videojuego> buscarPorDistribuidor(int id_db);
     
 } 
     
